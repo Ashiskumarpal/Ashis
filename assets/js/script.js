3c4a67e774af5111a2b5fc8703a254286cfefec1
Ashiskumarpal <86200350+Ashiskumarpal@users.noreply.github.com>
2023-10-19 23:39:11 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -160,51 +160,51 @@ for (let i = 0; i < navigationLinks.length; i++) {
 
 // Image Slider
 let sliderImages = document.querySelectorAll(".slide"),
-    arrowLeft = document.querySelector("#arrow-left"),
-    arrowRight = document.querySelector("#arrow-right"),
-    current = 0;
- 
+  arrowLeft = document.querySelector("#arrow-left"),
+  arrowRight = document.querySelector("#arrow-right"),
+  current = 0;
+
 // Clear all images
 function reset() {
-    for (let i = 0; i < sliderImages.length; i++) {
-        sliderImages[i].style.display = "none";
-    }
+  for (let i = 0; i < sliderImages.length; i++) {
+    sliderImages[i].style.display = "none";
+  }
 }
- 
-// Initial slide
+
+// Init slider
 function startSlide() {
-    reset();
-    sliderImages[0].style.display = "block";
+  reset();
+  sliderImages[0].style.display = "block";
 }
- 
-// Show previous
+
+// Show prev
 function slideLeft() {
-    reset();
-    sliderImages[current - 1].style.display = "block";
-    current--;
+  reset();
+  sliderImages[current - 1].style.display = "block";
+  current--;
 }
- 
+
 // Show next
 function slideRight() {
-    reset();
-    sliderImages[current + 1].style.display = "block";
-    current++;
+  reset();
+  sliderImages[current + 1].style.display = "block";
+  current++;
 }
- 
+
 // Left arrow click
-arrowLeft.addEventListener("click", function () {
-    if (current === 0) {
-        current = sliderImages.length;
-    }
-    slideLeft();
+arrowLeft.addEventListener("click", function() {
+  if (current === 0) {
+    current = sliderImages.length;
+  }
+  slideLeft();
 });
- 
+
 // Right arrow click
-arrowRight.addEventListener("click", function () {
-    if (current === sliderImages.length - 1) {
-        current = -1;
-    }
-    slideRight();
+arrowRight.addEventListener("click", function() {
+  if (current === sliderImages.length - 1) {
+    current = -1;
+  }
+  slideRight();
 });
- 
+
 startSlide();
