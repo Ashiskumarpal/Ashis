291034eaec4ec78427604629b74883d96f8a2bcd
Ashiskumarpal <86200350+Ashiskumarpal@users.noreply.github.com>
2023-10-20 00:44:09 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -157,7 +157,7 @@ for (let i = 0; i < navigationLinks.length; i++) {
 
   });
 }
-
+//----------------------------------->>>><<<<-------------------------------------
 // Image Slider
 let sliderImages = document.querySelectorAll(".slide"),
   arrowLeft = document.querySelector("#arrow-left"),
@@ -180,31 +180,47 @@ function startSlide() {
 // Show prev
 function slideLeft() {
   reset();
-  sliderImages[current - 1].style.display = "block";
-  current--;
+  if (current === 0) {
+    current = sliderImages.length - 1;
+  } else {
+    current--;
+  }
+  sliderImages[current].style.display = "block";
 }
 
 // Show next
 function slideRight() {
   reset();
-  sliderImages[current + 1].style.display = "block";
-  current++;
+  if (current === sliderImages.length - 1) {
+    current = 0;
+  } else {
+    current++;
+  }
+  sliderImages[current].style.display = "block";
 }
 
 // Left arrow click
 arrowLeft.addEventListener("click", function() {
-  if (current === 0) {
-    current = sliderImages.length;
-  }
   slideLeft();
 });
 
 // Right arrow click
 arrowRight.addEventListener("click", function() {
-  if (current === sliderImages.length - 1) {
-    current = -1;
-  }
   slideRight();
+});
+
+// Automatic slide every 3 seconds
+let autoSlide = setInterval(slideRight, 3000); // 3000 milliseconds (3 seconds)
+
+// Stop auto slide when the user interacts with the slider
+sliderImages.forEach(slide => {
+  slide.addEventListener("mouseover", function() {
+    clearInterval(autoSlide);
+  });
+
+  slide.addEventListener("mouseout", function() {
+    autoSlide = setInterval(slideRight, 3000);
+  });
 });
 
 startSlide();
